7a82b6407c620b819953800ded20e15e4cda1aa2
Suriya R <136679158+suriya276@users.noreply.github.com>
2024-03-17 09:58:24 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb : 1,
+        question: "heat transfer types?",
+        answer: "C. Three",
+        options : [
+        "A. One",
+        "B. Two",
+        "C. Three",
+        "D. Four"
+            
+        ]
+    },
+    {
+        numb : 2,
+        question: "What does HMT stands for ?",
+        answer: "D. Heat and Mass Transfer",
+        options : [
+            "A. Heat and molecules transfer",
+            "B. Heat and measure transfer",
+            "C. Heat and measure transform",
+            "D. Heat and Mass Transfer"
+
+        ]
+    },
+    {
+        numb : 3,
+        question: "what does CAM stands for?",
+        answer: "A. Computer Aided Manufacturing",
+        options : [
+            "A. Computer Aided Manufacturing",
+            "B. Computer Aided Machining",
+            "C. Computer Aided Manufacture",
+            "D. Computer Applied Manufacturing"
+
+        ]
+    },
+    {
+        numb : 4,
+        question: "what does CAD stands for?",
+        answer: "B. Computer Aided Design",
+        options : [
+            "A. Computer applied design",
+            "B. Computer Aided Design",
+            "C. Computer Aided department",
+            "D. Computer Aided Deform"
+        ]
+    },
+    {
+        numb : 5,
+        question: "What if the full form of GF?",
+        answer: "A. Gauge Factor",
+        options : [
+            "A. Gauge Factor", 
+            "B. Gear Factor",
+            "C. Gear Foot ",
+            "D. Gauge Foot"
+        ]
+    }
+];
